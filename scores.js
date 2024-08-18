64d7e1d822adb3fe7acc92216f7cc986d6a61bcf
Shado Terro <39624026+Beginning0@users.noreply.github.com>
2024-08-18 06:02:32 -0400

    Delete scores.js
    Signed-off-by: Shado Terro <39624026+Beginning0@users.noreply.github.com>

--- scores.js
+++
@@ -1,9 +0,0 @@
-function saveScore(name, score) {
-    let scores = JSON.parse(localStorage.getItem('scores')) || [];
-    scores.push({ name, score });
-    localStorage.setItem('scores', JSON.stringify(scores));
-}
-
-function getScores() {
-    return JSON.parse(localStorage.getItem('scores')) || [];
-}
